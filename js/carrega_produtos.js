706321701fb5da7ea5b15093eb9aa99c2c59e579
Lucas Alberto <lucas.a.santos126@aluno.senai.br>
2026-07-06 20:54:53 -0300

    script para o carrinho de compras

--- js/carrega_produtos.js
+++ js/carrega_produtos.js
@@ -1,10 +1,10 @@
 import {produtos} from "./produtos.js";
 
 //Pegando elementos do DOM
-const section_cards = document.querySelector(#cards)
+const section_cards = document.querySelector('#cards')
 
 //Função para carregar os produtos
-const listaProdutos = ()=>{
+const listarProdutos = ()=>{
     section_cards.innerHTML = ''
 
     produtos.forEach((elem, i)=>{
@@ -19,12 +19,21 @@ const listaProdutos = ()=>{
         const h2Titulo = document.createElemnt('h2')
         h2Titulo.innerHTML = elem.descricao_produto
 
-        const divValor = document.createElement('div')
-        divValor.setAttribute('class', 'valor_card')
-        divValor.innerHTML `R$ ${parseFloat(elem.valor_unitario).toFixed(2).replace('.',',')}`
+        const h3Valor = document.createElement('h3')
+        h3Valor.setAttribute('class', 'valor_card')
+        h3Valor.innerHTML `R$ ${parseFloat(elem.valor_unitario).toFixed(2).replace('.',',')}`
 
         const btnCard = document.createElement('button')
         btnCard.setAttribute('class', 'btn_card')
         btnCard.innerHTML = 'Adicionar'
+
+        divCard.appendChild(imgProduto)
+        divCard.appendChild(h2Titulo)
+        divCard.appendChild(h3Valor)
+        divCard.appendChild(btnCard)
+
+        section_cards.appendChild(divCard)
     });
 }
+
+listarProdutos()
