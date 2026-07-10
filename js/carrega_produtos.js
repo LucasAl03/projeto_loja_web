8e31565616d47cbdc13548f6a8659e5fc0207f88
Lucas Alberto <lucas.a.santos126@aluno.senai.br>
2026-07-10 18:59:12 -0300

    Codigo do link de filtragem

--- js/carrega_produtos.js
+++ js/carrega_produtos.js
@@ -4,14 +4,23 @@ import { produtos } from "./produtos.js";
 //PEGANDO ELEMENTO DO DOM
 const section_cards = document.querySelector('#cards')
 
-//FUNÇÃO PARA CARREGAR OS PRODUTOS
-const listarProdutos = () => {
-    section_cards.innerHTML = ''
+//CARREGA 
+const carregaProduto = (id_secao) => {
+    //AO CHAMAR A FUNÇÃO carregaProduto() DEVE PASSAR O PARÂMETRO. 0(ZERO) CHAMA A FUNÇÃO listarProdutos(), QUALQUER OUTRO VALOR CHAMA A FUNLÇAO produtosFiltrados(id_secao)
+    if (id_secao === 0) {
+        montandoCards(listarProdutos())
+    } else {
+        montandoCards(produtosFiltrados(id_secao))
+    }
 
-
+    //QUALQUER FUNÇÃO CHAMADA SEMPRE CHAMA A FUNÇÃO montarSecoes()
+    montarSecoes()
 }
 
-listarProdutos()
+//FUNÇÃO PARA CARREGAR OS PRODUTOS
+const listarProdutos = () => {
+    return produtos
+}
 
 //FILTRANDO AS SEÇÕES COM A COLEÇÃO map
 const listarSecoes = () => {
@@ -29,7 +38,6 @@ const listarSecoes = () => {
 
     //RETORNADO O ARRAY CONVERTIDO
     return secoesMenu
-
 }
 
 //MONTANDO OS LINKS SEÇÕES
@@ -38,6 +46,27 @@ const montarSecoes = () => {
     const ulMenu = document.querySelector('#menu-secoes')
     //LIMPANDO O ELEMENTO ulMenu
     ulMenu.innerHTML = ''
+
+    //CRIANDO O LINK TODO
+    //CRIANDO O ELEMENTO li
+    const liSecao = document.createElement('li')
+
+    //CRIANDO O ELEMENTO a
+    const aSecao = document.createElement('a')
+    aSecao.setAttribute('href', '#')
+    aSecao.setAttribute('class', 'lnk-secao')
+    aSecao.innerHTML = 'TODOS'
+    //CAPTURANDO O CLICK DOS LINKS
+    aSecao.addEventListener('click', () => {
+        //CHAMANDO A FUNÇÃO PRODUTOS FILTRADOS
+        carregaProduto(0)
+    })
+
+    //ADICIONANDO O ELEMENTO FILHO a NO ELEMENTO li
+    liSecao.appendChild(aSecao)
+
+    //ADICIONANDO O ELEMENTO FILHO li NO ELEMENTO DO DOM ul
+    ulMenu.appendChild(liSecao)
 
     //PERCORRENDO O ARRAY DAS SEÇÕES FILTRADA
     listarSecoes().forEach((elem, i) => {
@@ -56,7 +85,6 @@ const montarSecoes = () => {
             montandoCards(produtosFiltrados(elem.id_secao))
         })
 
-
         //ADICIONANDO O ELEMENTO FILHO a NO ELEMENTO li
         liSecao.appendChild(aSecao)
 
@@ -65,8 +93,6 @@ const montarSecoes = () => {
     })
 
 }
-
-montarSecoes()
 
 //FILTRANDO PRODUTOS 
 const produtosFiltrados = (idSecao) => {
@@ -106,3 +132,5 @@ const montandoCards = (objProdutos) => {
 
     })
 }
+
+carregaProduto(0)
