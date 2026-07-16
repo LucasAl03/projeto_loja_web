b0b2a1d2626c8ad4ab1bf34d50d63728726bf684
Lucas Alberto <lucas.a.santos126@aluno.senai.br>
2026-07-16 20:59:25 -0300

    Código que adiciona em quantidade se um item igual for adicionado ao carrinho e o codigo para mudar a quantidade no carrinho

--- js/carrinho.js
+++ js/carrinho.js
@@ -13,20 +13,29 @@ const fobjItem = (objProduto) => {
     descricao_produto: objProduto.descricao_produto,
     caminho_da_imagem: objProduto.caminho_da_imagem,
     valor_unitario: objProduto.valor_unitario,
-    quantidade: 2
+    quantidade: 1
     }
 
     return item
 } 
 
 //PEGANDO O INDICE DO ARRAY
-//console.log(itensCarrinho.findIndex(elem => elem.id_produto === elem.id_produto))
+//console.log(itensCarrinho.findIndex(elem => elem.id_produto == 2))
 
 //itensCarrinho.findIndex(elem => elem.id_produto === elem.id_produto)
 
 //FUNÇÃO PARA ADICIONAR O ITEM ARRAY
 const addItem = (objItem) => {
-    itensCarrinho.push(fobjItem(objItem))
+    //PROCURA SE O ITEM JA EXITE NO CARRINHO
+    const index = itensCarrinho.findIndex(elem => elem.id_produto === objItem.id_produto)
+
+    if (index !== -1) {
+        //SE JA EXISTE AUMENTA A QUANTIDADE
+        itensCarrinho[index].quantidade += 1
+    } else {
+        //SE NAO EXISTE ADICIONA O ITEM EM UMA NOVA LINHA
+        itensCarrinho.push(fobjItem(objItem))
+    }
 
     localStorage.setItem('itensSessao', JSON.stringify(itensCarrinho))
     //sessionStorage.setItem('itensSessao', JSON.stringify(itensCarrinho))
@@ -50,4 +59,18 @@ const removeItem = (pos)=>{
     //sessionStorage.setItem('itensSessao', JSON.stringify(itensCarrinho))
 }
 
-export { addItem, listItens, removeItem }
+//DEFINIR QUANTIDADE
+//pos - posição do item dentro da array
+const defineQuant = (pos, novaQuant) => {
+    //SE O INPUT quantidade ZERAR O ITEM SERÁ REMOVIDO
+    if (novaQuant < 1){
+        itensCarrinho.splice(pos, 1)
+    //ALTERA A PROPRIEDADE quantidade PELO NOVO VALOR
+    } else {
+        itensCarrinho[pos].quantidade = novaQuant
+    }
+
+    localStorage.setItem('itensSessao', JSON.stringify(itensCarrinho))
+}
+
+export { addItem, listItens, removeItem, defineQuant }
