7f6f26ddd08d5cab212e83252d78e8ed34504026
Lucas Alberto <lucas.a.santos126@aluno.senai.br>
2026-07-17 19:59:48 -0300

    correção no codigo

--- js/consulta_cep.js
+++ js/consulta_cep.js
@@ -4,25 +4,30 @@ const inputCep = document.querySelector('#cep')
 //CAPTURANDO O EVENTO AO PERDER O FOCO
 inputCep.addEventListener('change',(evt)=>{
     //PEGANDO OS NÚMEROS DO INPUT NÃO PERMITINDO OUTRO TIPO DE DAOS QUE NÃO NÚMEROS
-    const numCep = evt.target.value.replace(/\D/g,'')
+    const numCep = evt.target.value.replace(/\D/g, "")
 
     //VERIFICA SE SÃO 8 DÍGITOS
-    if(numCep != 8){
+    if(numCep.length != 8){
         alert('CEP INVÁLIDO !!!')
         return
     }
 
     //CHAMA A FUNÇÃO buscaDadosCep
-    buscandoDadosCep(numCep)
+    buscaDadosCep(numCep)
 })
 
+//await - espera
+//async - não para o processo enquanto a consulta é feita
+//fetch - nativo do java para buscar api fora do sistema
+
 //BUSCAR OS DADOS DO CEPS
-const buscandoDadosCep = async (cep) =>{
+const buscaDadosCep = async (cep) =>{
     //TENTA BUSCAR OS DADOS NO VIACEP
     try{
+        
         //BUSCA OS DADOS NO VIACEP
-        const response = await fetch(`https://viacep.com.br/ws/${cep}}/json/`)
-
+        const response = await fetch (`https://viacep.com.br/ws/${cep}/json/`)
+        
         //CONVERTE OS DADOS NO FORMATO json
         const dadosEndereco = await response.json()
 
@@ -30,13 +35,13 @@ const buscandoDadosCep = async (cep) =>{
         exibeDados(dadosEndereco)
     
     //CASO HAJA ALGUM ERRO É CAPTURAD PELO catch
-    } catch(erro){
-        console.log(erro.message)
+    } catch(erro) {
+        console.log('ERRO APRESENTADO', erro.message)
     }
 
 }
 
-//OBJETO LITERAL CAMPOS QUE CRIA CADA CHAVE SEJA UM INPUT DO DOM
+//OBJETO LITERAL CAMPOS QUE CADA CHAVE REPRESENTA OS INPUTS DO DOM
 const campos ={
     logradouro: document.querySelector('#logradouro'),
     bairro: document.querySelector('#bairro'),
@@ -46,16 +51,29 @@ const campos ={
 
 //FUNÇÃO EXIBE DADOS
 const exibeDados = (objDados)=>{
+
     //PEGA A DIV PAI DOS ELEMNTOS DO ENDEREÇO
     const divEndereco = document.querySelector('#div-dados-endereco')
+
     //REMOVE DA DIV O CLASS OCULTO
     divEndereco.classList.remove('oculto')
 
-    //PERCORRE O ONJETO, NO FORMATO JSON, DO VIDA CEP
-    for(let chave in objDados){
+    document.querySelector('#logradouro').value = objDados.logradouro
+    document.querySelector('#logradouro').disabled = true
+    document.querySelector('#bairro').value = objDados.logradouro
+    document.querySelector('#bairro').disabled = true
+    document.querySelector('#localidade').value = objDados.logradouro
+    document.querySelector('#localidade').disabled = true
+    document.querySelector('#uf').value = objDados.logradouro
+    document.querySelector('#uf').disabled = true
+
+    //PERCORRE O OBJETO, NO FORMATO JSON, DO VIDA CEP
+    /*for(let chave in objDados){
+
         //ATRIBUI O VALOR AO INPUT
         campos[chave].value = objDados[chave]
+
         //BLOQUEIA OS INPUTS. NÃO PERMITE QUE O USUÁRIO APAGUE OS VALORES
         campos[chave].disabled = objDados[chave]
-    }
+    }*/
 }
